d513078f667b58f0e3f1d90f0014c68d4104a8d1
Nicolás Quezada Baier <david.avila@uach.cl>
2025-04-06 18:07:03 -0400

    limpieza

--- modulos/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Inicializador del paquete de módulos.
-"""
-
-# Este archivo hace que Python trate el directorio como un paquete
-# permitiendo importar los módulos con: from modulos import X
